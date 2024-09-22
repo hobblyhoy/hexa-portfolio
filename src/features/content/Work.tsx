7f8e4a5cd074c0565907aeb86a906160c55192b9
Nathan <hobblyhoy@gmail.com>
2024-09-21 17:09:51 -0700

    tinkering with job options

--- src/features/content/Work.tsx
+++ src/features/content/Work.tsx
@@ -1,41 +1,49 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import Header from '../displayElements/Header';
+import Job, { IJobProps } from '../displayElements/Job';
 
 function Work() {
+   const jobs: IJobProps[] = [
+      {
+         title: 'Software Engineer',
+         companyName: 'iGrad Financial Wellness',
+         duration: '2019 => Current',
+         description: 'TODO',
+         tags: [
+            'JavaScript',
+            'TypeScript',
+            'React',
+            'C#',
+            '.NET',
+            'Cosmos',
+            'SQL Server',
+         ],
+      },
+      {
+         title: 'Software Developer',
+         companyName: 'MREN',
+         duration: '2016 => 2018',
+         description: 'TODO',
+         tags: ['JavaScript', 'Knockout', 'C#', 'ASP.NET', 'SQL Server', 'Selenium'],
+      },
+      {
+         title: 'Business Intelligence and IT Administrator',
+         companyName: 'iTAN Franchising',
+         duration: '2012 => 2016',
+         description: 'TODO',
+         tags: ['SQL Server', 'SSRS', 'IT'],
+      },
+   ];
+
    return (
       <div className="mt-24" id="work">
          <Header text="Work" />
-         <div>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Quisque orci proin
-            faucibus lacinia fermentum sapien; netus sapien potenti. Sapien eleifend massa
-            ad dignissim commodo vitae hac in. Adis ut tincidunt efficitur gravida nibh
-            condimentum erat curae. Himenaeos lacus aptent tincidunt mus penatibus
-            suspendisse. Diam aliquet pulvinar nullam amet scelerisque sodales at justo.
-            Condimentum maecenas vel mus felis elementum est phasellus. Fermentum euismod
-            metus venenatis pharetra convallis. Nostra nascetur bibendum donec duis cras
-            magna mattis.
-         </div>
-         <div>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Quisque orci proin
-            faucibus lacinia fermentum sapien; netus sapien potenti. Sapien eleifend massa
-            ad dignissim commodo vitae hac in. Adis ut tincidunt efficitur gravida nibh
-            condimentum erat curae. Himenaeos lacus aptent tincidunt mus penatibus
-            suspendisse. Diam aliquet pulvinar nullam amet scelerisque sodales at justo.
-            Condimentum maecenas vel mus felis elementum est phasellus. Fermentum euismod
-            metus venenatis pharetra convallis. Nostra nascetur bibendum donec duis cras
-            magna mattis.
-         </div>
-         <div>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Quisque orci proin
-            faucibus lacinia fermentum sapien; netus sapien potenti. Sapien eleifend massa
-            ad dignissim commodo vitae hac in. Adis ut tincidunt efficitur gravida nibh
-            condimentum erat curae. Himenaeos lacus aptent tincidunt mus penatibus
-            suspendisse. Diam aliquet pulvinar nullam amet scelerisque sodales at justo.
-            Condimentum maecenas vel mus felis elementum est phasellus. Fermentum euismod
-            metus venenatis pharetra convallis. Nostra nascetur bibendum donec duis cras
-            magna mattis.
-         </div>
+         {jobs.map(job => (
+            <div className="mb-4" key={job.title}>
+               <Job {...job} />
+            </div>
+         ))}
       </div>
    );
 }
